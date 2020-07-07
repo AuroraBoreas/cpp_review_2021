e205940afa7a8135038f89a9c2c17818150d0632
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2020-07-07 16:20:54 +0800

    Add files via upload

---
+++ cpp_11_chartype/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    char ch;
+
+    cout << "please enter a character!" << endl;
+    cin >> ch;
+    cout << "Hola, thank your for your character "
+         << ch
+         << endl;
+
+
+    return 0;
+}
