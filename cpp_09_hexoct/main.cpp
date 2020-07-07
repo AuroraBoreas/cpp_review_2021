2f253243b89e1724c79eb443ca4217168984e62b
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2020-07-07 16:19:57 +0800

    Add files via upload

---
+++ cpp_09_hexoct/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    int chest = 42;
+    int waist = 042;
+    int inseam = 0x42;
+
+    cout << "Moisture cuts a striking figure!" << endl;
+    cout << "chest  = " << chest << " (42 in decimal)" << endl;
+    cout << "waist  = " << waist << " (042 in octal)" << endl;
+    cout << "inseam = " << inseam << " (0x42 in hex)" << endl;
+
+    return 0;
+}
