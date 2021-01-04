74a339ab84b3067302624fa303ab5c4846749e85
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-01-04 13:57:46 +0800

    Add files via upload

--- cpp_07_Accelerated+C++/src_code/chapter-03-WorkWithBatchesOfData/advanced_c++_review14/main.cpp
+++ cpp_07_Accelerated+C++/src_code/chapter-03-WorkWithBatchesOfData/advanced_c++_review14/main.cpp
@@ -20,18 +20,84 @@ normal cls vs composition cls
 
 using namespace std;
 
-class BaseDog{};
+class Activity
+{
+    public:
+        virtual void bark(void) const = 0;
+        virtual void call(void) const = 0;
+};
 
-class YellowDog: public BaseDog{};
+class BaseDog: public Activity
+{
+    public:
+        BaseDog(){ cout << "Base Dog born" << endl; }
+        ~BaseDog(){ cout << "Base Dog destroyed" << endl; }
+        void bark(void) const
+        {
+            cout << "Base Dog is barking" << endl;
+        }
+        void call(void) const
+        {
+            cout << "Base Dog is calling" << endl;
+        }
+};
 
-class GreenDog: public BaseDog{};
+class YellowDog: public Activity
+{
+public:
+    YellowDog(){ cout << "Yellow Dog born" << endl; }
+    ~YellowDog(){ cout << "Yellow Dog destroyed" << endl; }
+    void bark(void) const
+    {
+        cout << "Yellow Dog is barking" << endl;
+    }
+    void call(void) const
+    {
+        cout << "Yellow Dog is calling" << endl;
+    }
+};
 
-class SuperiorDog: public YellowDog, public BaseDog{};
+class GreenDog: public Activity
+{
+public:
+    GreenDog(){ cout << "Green Dog born" << endl; }
+    ~GreenDog(){ cout << "Green Dog destroyed" << endl; }
+    void bark(void) const
+    {
+        cout << "Green Dog is barking" << endl;
+    }
+    void call(void) const
+    {
+        cout << "Green Dog is calling" << endl;
+    }
+};
+
+class SuperiorDog: public BaseDog
+{
+public:
+    SuperiorDog(){ cout << "Superior Dog born" << endl; }
+    ~SuperiorDog(){ cout << "Superior Dog destroyed" << endl; }
+    void bark(void) const
+    {
+        cout << "Superior Dog is barking" << endl;
+    }
+    void call(void) const
+    {
+        cout << "Superior Dog is calling" << endl;
+    }
+};
 
 int main()
 {
+    // BaseDog d1(); // not OK. its a function with return_type BaseDog..
+    // or
+    BaseDog d1 = BaseDog();
+    d1.bark();
 
+    YellowDog* d2 = new YellowDog();
+    d2->call();
 
+    delete d2;
 
     return 0;
 }
