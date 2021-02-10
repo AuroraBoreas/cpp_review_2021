c1770c52ba560e640653d4b9d88aa6f66579e197
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-02-10 16:15:34 +0800

    Add files via upload

---
+++ BQ-Modern-C++/c++_vs_c#_class_concept/Base.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoNS
+{
+    public class Base
+    {
+        public int Number { get; set; } = 42;
+
+        public void A()
+        { Console.WriteLine("Base class A method"); }
+
+        public virtual void B(int i)
+        { Console.WriteLine($"Base class B method, {i}"); }
+    }
+}
---
+++ BQ-Modern-C++/c++_vs_c#_class_concept/Derived.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoNS
+{
+    public class Derived : Base
+    {
+        public new int Number { get; set; } = 12;
+
+        public new void A()
+        { Console.WriteLine("Derived class A method"); }
+
+        public override void B(int i)
+        => Console.WriteLine("Derived class B method, {0}", ++i);
+    }
+}
